106abcdf83505fad9b8008835dbae08ac4d74885
hubin <hubin.android@gmail.com>
2023-12-15 10:36:26 +0800

    FloatingWindow 依赖 Common 添加日志

--- module/FloatingWindow/src/main/kotlin/online/testdata/player/floatingwindow/initializer/PlayerXFloatingWindowInitializer.kt
+++ module/FloatingWindow/src/main/kotlin/online/testdata/player/floatingwindow/initializer/PlayerXFloatingWindowInitializer.kt
@@ -2,11 +2,12 @@ package online.testdata.player.floatingwindow.initializer
 
 import android.content.Context
 import androidx.startup.Initializer
+import online.testdata.player.common.logger.smartLog
 
-class PlayerFloatingWindowInitializer : Initializer<Unit> {
+class PlayerXFloatingWindowInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
-
+        smartLog(tag = "Initializer") { "PlayerXFloatingWindow 模块初始化完成" }
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {
