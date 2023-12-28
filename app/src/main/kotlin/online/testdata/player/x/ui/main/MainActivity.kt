61199066cd10ad0b2b3b187c41a0d6999431683d
hubin <hubin.android@gmail.com>
2023-12-28 16:29:16 +0800

    TensorFlow 图像识别使用悬浮窗画框

--- app/src/main/kotlin/online/testdata/player/x/ui/main/MainActivity.kt
+++ app/src/main/kotlin/online/testdata/player/x/ui/main/MainActivity.kt
@@ -1,13 +1,13 @@
 package online.testdata.player.x.ui.main
 
 import android.content.Intent
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.hjq.window.EasyWindow
 import dagger.hilt.android.AndroidEntryPoint
 import online.testdata.player.accessibility.service.PlayerXAccessibilityService
 import online.testdata.player.accessibility.service.PlayerXForegroundService
@@ -19,10 +19,9 @@ import online.testdata.player.screenshare.PlayerXScreenShare.startScreenShare
 import online.testdata.player.tf.BitmapUtil
 import online.testdata.player.tf.DetectTool
 import online.testdata.player.tf.NonMaxSuppression
+import online.testdata.player.x.R
 import online.testdata.player.x.databinding.ActivityMainBinding
 import org.tensorflow.lite.Interpreter
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
 import java.util.Arrays
 
 
@@ -31,10 +30,19 @@ class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val interpreter: Interpreter? by lazy { DetectTool.getInterpreter(this) }
+    private val easyWindow by lazy { EasyWindow.with(application) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initView()
+
+        easyWindow
+            .setContentView(R.layout.floating_window)
+            .setAnimStyle(-1)
+            .setImageDrawable(R.id.floating_window_icon, R.mipmap.ic_launcher)
+            .setWindowFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            .setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN)
+            .show()
     }
 
     override fun onResume() {
@@ -65,8 +73,73 @@ class MainActivity : AppCompatActivity() {
                 binding.buttonForegroundService.text = "已开启"
             }
         }
+        var working = false
         binding.buttonScreenShare.setOnClickListener {
-            startScreenShare()
+            startScreenShare { bitmap ->
+                if(working) {
+                    smartLogDebug { "ScreenShare-Skip" }
+                    return@startScreenShare
+                }
+                working = true
+                var startMS = System.currentTimeMillis()
+
+                val resizeBitmap = BitmapUtil.resizeBitmap(bitmap, 640)
+                runOnUiThread {
+                    //binding.dnfImage.setImageBitmap(bitmap)
+                }
+                val inputByteBuffer = BitmapUtil.convertBitmapToByteBuffer(resizeBitmap)
+                // 构建一个空的输出结构
+                val outArray = Array(1) {
+                    Array(7) {
+                        FloatArray(8400)
+                    }
+                }
+                // 运行解释器，input_arr是输入，它会将结果写到outArray中
+                interpreter?.run(inputByteBuffer, outArray)
+                // 取出(1, 7, 8400)中的(7, 8400)
+                val matrix_2d = outArray[0]
+                // (7, 8400)变为(8400, 7)
+                val outputMatrix = Array(8400) {
+                    FloatArray(
+                        7
+                    )
+                }
+                for (i in 0..8399) {
+                    for (j in 0..6) {
+                        outputMatrix[i][j] = matrix_2d[j][i]
+                    }
+                }
+                val threshold = 0.6f // 类别准确率筛选
+
+                val non_max = 0.8f // nms非极大值抑制
+
+                val boxes = ArrayList<FloatArray>()
+                val maxScores: ArrayList<Float> = ArrayList()
+                for (detection in outputMatrix) {
+                    // 6位数中的后两位是两类的置信度
+                    val score = Arrays.copyOfRange(detection, 4, 7)
+                    var maxValue = score[0]
+                    var maxIndex = 0f
+                    for (i in 1 until score.size) {
+                        if (score[i] > maxValue) { // 找出最大的一项
+                            maxValue = score[i]
+                            maxIndex = i.toFloat()
+                        }
+                    }
+                    if (maxValue >= threshold) { // 如果置信度超过60%则记录
+                        detection[4] = maxIndex
+                        detection[5] = maxValue
+                        boxes.add(detection) // 筛选后的框
+                        maxScores.add(maxValue) // 筛选后的准确率
+                    }
+                }
+                val nonMaxBoxes = NonMaxSuppression.nonMaxSuppression(boxes, maxScores, non_max)
+                smartLogDebug { "ScreenShare: ${System.currentTimeMillis() - startMS}MS" }
+                smartLogDebug { "ScreenShare: ${boxes.size}, scores: ${maxScores.size}, nonMaxBoxes: ${nonMaxBoxes.size}" }
+
+                easyWindow.findViewById<DetectBoxView>(R.id.box_view).boxes = adjustToRealBox(nonMaxBoxes)
+                working = false
+            }
         }
         binding.buttonTensorFlow.setOnClickListener {
             var startMS = System.currentTimeMillis()
@@ -79,69 +152,116 @@ class MainActivity : AppCompatActivity() {
             startMS = System.currentTimeMillis()
             binding.dnfImage.setImageBitmap(resizeBitmap)
             // 转换为输入层(1, 640, 640, 3)结构的float数组
-            smartLogDebug { "bitmap: ${bitmap.width},${bitmap.height}" }
-            val inputByteBuffer = BitmapUtil.convertBitmapToByteBuffer(resizeBitmap)
-            smartLogDebug { "bitmapToFloatArray: ${System.currentTimeMillis() - startMS}MS" }
-            startMS = System.currentTimeMillis()
-            // 构建一个空的输出结构
-            //
-            val outArray = Array(1) {
-                Array(7) {
-                    FloatArray(8400)
-                }
-            }
-            // 运行解释器，input_arr是输入，它会将结果写到outArray中
-            interpreter?.run(inputByteBuffer, outArray)
-            smartLogDebug { "tflite.interpreter: ${System.currentTimeMillis() - startMS}MS" }
-            startMS = System.currentTimeMillis()
-            smartLogDebug { "outArray: $outArray" }
+//            smartLogDebug { "bitmap: ${bitmap.width},${bitmap.height}" }
+//            val inputByteBuffer = BitmapUtil.convertBitmapToByteBuffer(resizeBitmap)
+//            smartLogDebug { "bitmapToFloatArray: ${System.currentTimeMillis() - startMS}MS" }
+//            startMS = System.currentTimeMillis()
+//            // 构建一个空的输出结构
+//            //
+//            val outArray = Array(1) {
+//                Array(7) {
+//                    FloatArray(8400)
+//                }
+//            }
+//            // 运行解释器，input_arr是输入，它会将结果写到outArray中
+//            interpreter?.run(inputByteBuffer, outArray)
+//            smartLogDebug { "tflite.interpreter: ${System.currentTimeMillis() - startMS}MS" }
+//            startMS = System.currentTimeMillis()
+//            smartLogDebug { "outArray: $outArray" }
+//
+//            // 取出(1, 7, 8400)中的(7, 8400)
+//            val matrix_2d = outArray[0]
+//            // (7, 8400)变为(8400, 7)
+//            val outputMatrix = Array(8400) {
+//                FloatArray(
+//                    7
+//                )
+//            }
+//            for (i in 0..8399) {
+//                for (j in 0..6) {
+//                    outputMatrix[i][j] = matrix_2d[j][i]
+//                }
+//            }
+//            smartLogDebug { "for-for: ${System.currentTimeMillis() - startMS}MS" }
+//            startMS = System.currentTimeMillis()
+//            val threshold = 0.6f // 类别准确率筛选
+//
+//            val non_max = 0.8f // nms非极大值抑制
+//
+//            val boxes = ArrayList<FloatArray>()
+//            val maxScores: ArrayList<Float> = ArrayList()
+//            for (detection in outputMatrix) {
+//                // 6位数中的后两位是两类的置信度
+//                val score = Arrays.copyOfRange(detection, 4, 7)
+//                var maxValue = score[0]
+//                var maxIndex = 0f
+//                for (i in 1 until score.size) {
+//                    if (score[i] > maxValue) { // 找出最大的一项
+//                        maxValue = score[i]
+//                        maxIndex = i.toFloat()
+//                    }
+//                }
+//                if (maxValue >= threshold) { // 如果置信度超过60%则记录
+//                    detection[4] = maxIndex
+//                    detection[5] = maxValue
+//                    boxes.add(detection) // 筛选后的框
+//                    maxScores.add(maxValue) // 筛选后的准确率
+//                }
+//            }
+//            smartLogDebug { "box-filter: ${System.currentTimeMillis() - startMS}MS" }
+//            startMS = System.currentTimeMillis()
+//            val nonMaxBoxes = NonMaxSuppression.nonMaxSuppression(boxes, maxScores, non_max)
+//            smartLogDebug { "nonMaxBoxes: ${System.currentTimeMillis() - startMS}MS" }
+//            startMS = System.currentTimeMillis()
+//            smartLogDebug { "boxes: ${boxes.size}, scores: ${maxScores.size}, nonMaxBoxes: ${nonMaxBoxes.size}" }
+//            binding.boxView.boxes = nonMaxBoxes
+        }
 
-            // 取出(1, 7, 8400)中的(7, 8400)
-            val matrix_2d = outArray[0]
-            // (7, 8400)变为(8400, 7)
-            val outputMatrix = Array(8400) {
-                FloatArray(
-                    7
-                )
-            }
-            for (i in 0..8399) {
-                for (j in 0..6) {
-                    outputMatrix[i][j] = matrix_2d[j][i]
-                }
-            }
-            smartLogDebug { "for-for: ${System.currentTimeMillis() - startMS}MS" }
-            startMS = System.currentTimeMillis()
-            val threshold = 0.6f // 类别准确率筛选
+        binding.buttonFloatingWindow.setOnClickListener {
+            easyWindow
+                .setContentView(R.layout.floating_window)
+                .setAnimStyle(-1)
+                .setImageDrawable(R.id.floating_window_icon, R.mipmap.ic_launcher)
+                .setWindowFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_FULLSCREEN)
+                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN)
+                .show()
+        }
+    }
 
-            val non_max = 0.8f // nms非极大值抑制
+    private fun adjustToRealBox(list: List<FloatArray>): List<FloatArray> {
+        val resources = this.resources
+        val dm = resources.displayMetrics
+        val screenWidth = dm.widthPixels
+        val screenHeight = dm.heightPixels
+        val orientation = if(screenWidth < screenHeight) {
+            0 // 竖屏
+        } else {
+            1 // 横屏
+        }
+        return list.map { item ->
+            if(orientation == 0) {
+                val ratio = screenHeight / 640.0
+                val ratioWidth = screenWidth / ratio
+                val difRatio = 640.0 / ratioWidth
+                val singleWidth = (640 - ratioWidth) / 2
 
-            val boxes = ArrayList<FloatArray>()
-            val maxScores: ArrayList<Float> = ArrayList()
-            for (detection in outputMatrix) {
-                // 6位数中的后两位是两类的置信度
-                val score = Arrays.copyOfRange(detection, 4, 7)
-                var maxValue = score[0]
-                var maxIndex = 0f
-                for (i in 1 until score.size) {
-                    if (score[i] > maxValue) { // 找出最大的一项
-                        maxValue = score[i]
-                        maxIndex = i.toFloat()
-                    }
-                }
-                if (maxValue >= threshold) { // 如果置信度超过60%则记录
-                    detection[4] = maxIndex
-                    detection[5] = maxValue
-                    boxes.add(detection) // 筛选后的框
-                    maxScores.add(maxValue) // 筛选后的准确率
-                }
+                val mutableList = item.toMutableList()
+                // 中心点x, 中心点y, 宽度w, 高度h,
+                mutableList[0] = ((640 * item[0] - singleWidth)/ratioWidth).toFloat()
+                mutableList[2] = (mutableList[2] * difRatio).toFloat()
+                mutableList.toFloatArray()
+            } else {
+                val ratio = screenWidth / 640.0
+                val ratioHeight = screenHeight / ratio
+                val difRatio = 640.0 / ratioHeight
+                val singleHeight = (640 - ratioHeight) / 2
+
+                val mutableList = item.toMutableList()
+                // 中心点x, 中心点y, 宽度w, 高度h,
+                mutableList[1] = ((640 * item[1] - singleHeight)/ratioHeight).toFloat()
+                mutableList[3] = (mutableList[3] * difRatio).toFloat()
+                mutableList.toFloatArray()
             }
-            smartLogDebug { "box-filter: ${System.currentTimeMillis() - startMS}MS" }
-            startMS = System.currentTimeMillis()
-            val nonMaxBoxes = NonMaxSuppression.nonMaxSuppression(boxes, maxScores, non_max)
-            smartLogDebug { "nonMaxBoxes: ${System.currentTimeMillis() - startMS}MS" }
-            startMS = System.currentTimeMillis()
-            smartLogDebug { "boxes: ${boxes.size}, scores: ${maxScores.size}, nonMaxBoxes: ${nonMaxBoxes.size}" }
-            binding.boxView.boxes = nonMaxBoxes
         }
     }
 
--- module/ScreenShare/src/main/kotlin/online/testdata/player/screenshare/PlayerXScreenShare.kt
+++ module/ScreenShare/src/main/kotlin/online/testdata/player/screenshare/PlayerXScreenShare.kt
@@ -1,14 +1,20 @@
 package online.testdata.player.screenshare
 
+import android.R.attr
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import online.testdata.player.common.logger.smartLogDebug
 import org.loka.screensharekit.EncodeBuilder
 import org.loka.screensharekit.ScreenShareKit
 import org.loka.screensharekit.callback.RGBACallBack
+import java.nio.ByteBuffer
+
+
+typealias ScreenShareBitmapCallback = (Bitmap) -> Unit
 
 object PlayerXScreenShare {
 
-    fun AppCompatActivity.startScreenShare() {
+    fun AppCompatActivity.startScreenShare(bitmapCallback: ScreenShareBitmapCallback) {
         // 获取 RGBA 数据
         ScreenShareKit.init(this)
             .config(screenDataType = EncodeBuilder.SCREEN_DATA_TYPE.RGBA)
@@ -22,8 +28,30 @@ object PlayerXScreenShare {
                 rotation: Int,
                 rotationChanged: Boolean
             ) {
+                val rowStride = stride * 4
                 // 屏幕截图数据
-                smartLogDebug("PlayerXScreenShare") { "屏幕截图数据: $width,$height,$stride" }
+                smartLogDebug("PlayerXScreenShare") { "屏幕截图数据: $width,$height,$stride,$rotation" }
+                val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+                if(stride == width) {
+                    bitmap.copyPixelsFromBuffer(ByteBuffer.wrap(rgba))
+                } else {
+                    // If rowStride is greater than width, copy row by row
+                    //val pixelStride: Int = 4
+                    //val bitmapRowStride: Int = bitmap.rowBytes
+
+                    val bytesPerPixel: Int = 4 // 4 bytes per pixel (RGBA_8888)
+
+                    val buffer = ByteBuffer.wrap(rgba)
+
+                    val totalBuffer = ByteArray(width * bytesPerPixel * height)
+
+                    for (row in 0 until height) {
+                        buffer.position(row * rowStride)
+                        buffer.get(totalBuffer, row * width * bytesPerPixel, width * bytesPerPixel)
+                    }
+                    bitmap.copyPixelsFromBuffer(ByteBuffer.wrap(totalBuffer))
+                }
+                bitmapCallback(bitmap)
             }
 
         }).onStart {
